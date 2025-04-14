8568edbec125edd83e0944c8e3d96be3ddc7555c
TSM神山 <chizuru_koyama@tosho-m.co.jp>
2025-04-14 15:26:16 +0900

    14章対応完了

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,7 +8,6 @@ import {
   Revenue,
 } from "./definitions";
 import { formatCurrency } from "./utils";
-import { notFound } from "next/navigation";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
